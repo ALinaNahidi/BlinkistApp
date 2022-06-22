be513e2db3edbf83254cb4f81608490ef1f2a7f1
ALinaNahidi <abdul.alinakhanam@zemosolabs.com>
2022-06-22 19:37:00 +0530

    tests added for increasing coverage

--- src/components/Organisms/EntrepeneushipComponent/index.test.tsx
+++ src/components/Organisms/EntrepeneushipComponent/index.test.tsx
@@ -5,6 +5,7 @@ import { MemoryRouter } from "react-router-dom";
 import baseTheme from "../../../themes";
 import { getBooks } from "../../../Data/data";
 import EntrepeneushipPageComponent from ".";
+import CardStateTrending from "./cardState";
 
 afterEach(cleanup);
 const book = getBooks()[0];
@@ -27,3 +28,41 @@ it("renders Book Card author Organism", () => {
   expect(Librarybutton[0]).toBeInTheDocument();
   expect(Librarybutton[0]).toBeDefined();
 });
+
+
+it("renders Book Card author Organism2", () => {
+  render(
+    <ThemeProvider theme={baseTheme}>
+      <EntrepeneushipPageComponent bookObject={getBooks()} />
+    </ThemeProvider>,
+    {
+      wrapper: MemoryRouter,
+    }
+  );
+  const Librarybutton = screen.getAllByText(book.title);
+
+  expect(Librarybutton[0]).toBeTruthy();
+  expect(Librarybutton[0]).toHaveClass("MuiTypography-subtitle2");
+  expect(Librarybutton[0]).toBeInTheDocument();
+  expect(Librarybutton[0]).toBeDefined();
+});
+
+const handleFinish = () => {} 
+
+it("renders Book Card author Organism3", () => {
+  render(
+    <ThemeProvider theme={baseTheme}>
+      <CardStateTrending books={getBooks()} onFinishedClick={handleFinish} limit={3} type={["myLibrary"]} />
+    </ThemeProvider>,
+    {
+      wrapper: MemoryRouter,
+    }
+  );
+  const Librarybutton = screen.getAllByText(book.author);
+
+  expect(Librarybutton[0]).toBeTruthy();
+  expect(Librarybutton[0]).toHaveClass("MuiTypography-subtitle2");
+  expect(Librarybutton[0]).toBeInTheDocument();
+  expect(Librarybutton[0]).toBeDefined();
+});
+
